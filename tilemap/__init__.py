4229fe5652142d29d3402be5f60fe88e77a65a87
eunsoop <193854505+eunsoop@users.noreply.github.com>
2026-06-03 23:35:27 -0400

    struct: tilemap

---
+++ tilemap/__init__.py
@@ -0,0 +1,15 @@
+from pygame import Surface
+
+from scene import Layer
+
+
+class TilemapLayer(Layer):
+    def __init__(self):
+        super().__init__()
+        
+
+    def event(self, event):
+        pass
+
+    def paint(self, surface: Surface):
+        pass
